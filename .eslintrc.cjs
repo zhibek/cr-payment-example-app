02d14cdd5dae4e1f71eaab2a3743dc92f7b09eaa
John Levermore <john.levermore@zhibek.com>
2024-06-14 11:03:02 +0100

    Re-configure eslint as recommended by README

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,12 +3,29 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react',
+    'react-refresh',
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
